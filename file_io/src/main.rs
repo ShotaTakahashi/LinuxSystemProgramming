445986e9a22e848dfda4ea996b684706647d8f4a
ShotaTakahashi <takahashi.s.be@m.titech.ac.jp>
2018-10-01 23:14:22 +0900

    add select and poll codes

--- file_io/src/main.rs
+++ file_io/src/main.rs
@@ -1,9 +1,20 @@
+#![feature(mpsc_select)]
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::sync::{Arc, Mutex};
-use std::thread::spawn;
+use std::thread;
+use nix::sys::select;
+use nix::sys::time::{TimeValLike, TimeVal};
+use nix::poll::{EventFlags, PollFd, poll};
+
+extern crate libc;
+extern crate nix;
+
+const TIMEOUT: i64 = 5;
 
 fn main() {
+    // create, open, read, write
     let path = "test.txt";
 
     {
@@ -17,15 +28,66 @@ fn main() {
 
     assert_eq!(buffer, "hello world");
 
+
+    // seek
+    fd.seek(SeekFrom::Start(6)).unwrap();
+    let mut seek_buffer = String::new();
+    fd.read_to_string(&mut seek_buffer).unwrap();
+
+    assert_eq!(seek_buffer, "world");
+
+
+    // synchronized
+    let fd = File::open(&path).unwrap();
     let fd = Arc::new(Mutex::new(fd));
 
     for _ in 0..10 {
         let fd = fd.clone();
         let mut buffer = String::new();
-        spawn(move || {
+        thread::spawn(move || {
             let mut fd = fd.lock().unwrap();
             fd.read_to_string(&mut buffer).unwrap();
-            println!("{}", buffer);
+            println!("{:?}", buffer);
         });
     }
+
+
+    // select
+    let mut readfds = select::FdSet::new();
+    readfds.insert(libc::STDIN_FILENO);
+
+    let mut tv: TimeVal = TimeValLike::seconds(TIMEOUT);
+
+    let ret = select::select(Some(readfds.highest().unwrap() + 1),
+                   &mut readfds,
+                   None,
+                   None,
+                   &mut tv).unwrap();
+
+    match ret {
+        -1 => eprintln!("select"),
+        0 => println!("{} seconds elapsed.", TIMEOUT),
+        _ => {
+            if readfds.contains(libc::STDIN_FILENO) {
+                /*
+                 * Not Implemented
+                 */
+            }
+        }
+    }
+
+
+    // poll
+    let stdin = PollFd::new(libc::STDIN_FILENO, EventFlags::POLLIN);
+    let stdout = PollFd::new(libc::STDOUT_FILENO, EventFlags::POLLOUT);
+
+    let mut fds = [stdin, stdout];
+
+    let ret = poll(&mut fds, (TIMEOUT * 1000) as i32).unwrap();
+
+    if ret == -1 { println!("poll"); };
+    if ret == 0 { println!("TIMEOUT"); };
+
+    if fds[0].revents().unwrap().intersects(EventFlags::POLLIN) { println!("stdin is readable");};
+    if fds[1].revents().unwrap().intersects(EventFlags::POLLOUT) { println!("stdout is writable");}
 }
