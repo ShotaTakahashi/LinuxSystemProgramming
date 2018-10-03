bcb4619a909a2da444dc2f8ebcc42eec517646c3
ShotaTakahashi <takahashi.s.be@m.titech.ac.jp>
2018-10-03 22:59:57 +0900

    [WIP] There might be error in nix::sys::epoll

--- advanced_file_io/src/main.rs
+++ advanced_file_io/src/main.rs
@@ -3,19 +3,29 @@ extern crate nix;
 
 use nix::sys::uio::*;
 use nix::fcntl::*;
-use nix::fcntl::OFlag;
 use nix::sys::stat::Mode;
+
+//use nix::sys::epoll::{EpollCreateFlags, EpollEvent, EpollFlags, EpollOp};
+//use nix::sys::epoll::{epoll_create1, epoll_ctl};
+
+use nix::sys::mman::{};
+use nix::sys::mman::{mmap};
 
 use std::str;
 
 fn main() {
+    // scatter-gather I/O
+    // write
     let buffer =
         b"The term buccaneer comes from the word boucan.\n\
         A boucan is a wooden frame used for cooking meat.\n\
         Buccaneer is the West Indies name for a pirate.\n";
-    let fd = open("buccaneer.txt", OFlag::O_WRONLY, Mode::S_IWUSR).unwrap();
+    let fd = open("buccaneer.txt",
+                  OFlag::O_WRONLY | OFlag::O_CREAT | OFlag::O_TRUNC,
+                  Mode::S_IWUSR)
+            .unwrap();
 
-    let mut iov = [
+    let iov = [
         IoVec::from_slice(&buffer[0..45]),
         IoVec::from_slice(&buffer[45..96]),
         IoVec::from_slice(&buffer[96..145]),
@@ -24,6 +34,8 @@ fn main() {
     let nr = writev(fd , &iov).unwrap();
     println!("wrote {} bytes", nr);
 
+
+    // read
     let fd = open("buccaneer.txt", OFlag::O_RDONLY, Mode::S_IWUSR).unwrap();
 
     let mut buffers = vec![
@@ -40,4 +52,24 @@ fn main() {
         print!("{}: {}",i, str::from_utf8(&buffers[i]).unwrap());
     }
     println!("read {} bytes", nr);
+
+    /*
+    let fd = open("buccaneer.txt",
+                  OFlag::O_WRONLY | OFlag::O_CREAT | OFlag::O_TRUNC,
+                  Mode::S_IWUSR)
+        .unwrap();
+
+    let epfd = epoll_create1(EpollCreateFlags::empty()).unwrap();
+    let mut event = EpollEvent {
+        event: EpollFlags::EPOLLIN | EpollFlags::EPOLLOUT,
+    };
+
+    let ret = epoll_ctl(epfd, EpollOp::EpollCtlAdd, None, &mut event);
+
+    match ret {
+        Ok(_) => eprintln!("epoll_ctr"),
+        Err(_) => println!("not come")
+    }*/
+
+
 }
